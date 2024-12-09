213ab001273d92c3eee38edee7c557118fcf5943
Daniel Lang <ldaniell14260@gmail.com>
2024-12-10 00:13:43 +0100

    06 - Guard Gallivant - Part 1 - Optimized

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -4,124 +4,56 @@ defmodule AdventOfCode.Y2024.Day06 do
   def run(sample \\ true) do
     input =
       get_input(sample)
-      |> String.split()
+      |> String.split("\n")
 
     height = length(input)
     width = String.length(hd(input))
 
-    input =
+    obstacles =
       input
-      |> Enum.with_index()
-      |> Enum.reduce(Map.new(), &parse_row/2)
-
-    directionChars = [
-      [0, "^"],
-      [1, ">"],
-      [2, "v"],
-      [3, "<"]
-    ]
-
-    directions = [
-      {-1, 0},
-      {0, 1},
-      {1, 0},
-      {0, -1}
-    ]
-
-    start =
-      directionChars
-      |> Enum.flat_map(fn dir ->
-        [idx, char] = dir
-        pos = find_char(input, char)
-        case pos do
-          [] -> []
-          _ -> pos ++ [idx]
-        end
+      |> Stream.map(fn row -> String.split(row, "", trim: true) |> Enum.with_index() end)
+      |> Stream.with_index()
+      |> Stream.flat_map(fn {row, row_idx} ->
+        Enum.map(row, fn {col, col_idx} -> {row_idx, col_idx, col} end)
       end)
+      |> Enum.filter(fn {_row, _col, char} -> char != "." end)
 
-    [pos, dir] = start
+    [{start_row, start_col, _}] =
+      obstacles
+      |> Enum.filter(fn {_row, _col, char} -> char == "^" end)
 
-    input = Map.replace(input, pos, ".")
+    obstacles =
+      obstacles
+      |> List.delete({start_row, start_col, "^"})
+      |> Enum.map(fn {row, col, _} -> {row, col} end)
 
-    p1 =
-      move(pos, dir, input, width, height, directions, [])
+    visited =
+      move({start_row, start_col}, {-1, 0}, obstacles, width, height, [])
       |> Enum.uniq_by(fn {row, col} -> "#{row}-#{col}" end)
-      |> length()
 
-    positions = Enum.flat_map(0..height, fn row ->
-      Enum.map(0..width, fn col -> {row, col} end)
-    end)
+    p1 = length(visited)
 
-    p2 =
-        Task.async_stream(positions, &loops?(pos, dir, input, width, height, directions, &1))
-      |> Enum.reduce(0, fn {:ok, num}, acc -> num + acc end)
+    :persistent_term.put(Y2024Day06, {{start_row, start_col}, obstacles, width, height})
+
+    p2 = 0
 
     {p1, p2}
   end
 
-  defp loops?(start, dir, grid, width, height, directions, {rowObst, colObst}) do
-    case start == {rowObst, colObst} and Map.get(grid, {rowObst, colObst}, ".") != "#" do
-      true -> 0
-      false ->
-        modified_grid = Map.replace(grid, {rowObst, colObst}, "#")
-        move_p2(start, dir, modified_grid, width, height, directions, [])
+  defp move({row, col}, dir, obstacles, width, height, visited) when row >= 0 and row < height and col >= 0 and col < width do
+    visited = [{row, col} | visited]
+
+    {row_dir, col_dir} = dir
+    {row_next, col_next} = {row + row_dir, col + col_dir}
+
+    case obstacles |> Enum.find(nil, fn {row_ob, col_ob} -> row_ob == row_next and col_ob == col_next end) do
+      nil -> move({row_next, col_next}, dir, obstacles, width, height, visited)
+      _ -> move({row, col}, {col_dir, -row_dir}, obstacles, width, height, visited)
     end
   end
 
-  defp move_p2({row, col}, dir, grid, width, height, directions, visited) when row >= 0 and row < height and col >= 0 and col < width do
-    case Enum.member?(visited, {row, col, dir}) do
-      true -> 1
-      false ->
-        visited = [ {row, col, dir} | visited ]
-        {next_pos, next_dir} = get_next(grid, row, col, dir, directions)
-        move_p2(next_pos, next_dir, grid, width, height, directions, visited)
-    end
-  end
-
-  defp move_p2(_pos, _dir, _grid, _width, _height, _directions, _visited) do
-    0
-  end
-
-  defp move({row, col}, dir, grid, width, height, directions, visited) when row >= 0 and row < height and col >= 0 and col < width do
-    visited = [ {row, col} | visited ]
-
-    {next_pos, next_dir} = get_next(grid, row, col, dir, directions)
-    move(next_pos, next_dir, grid, width, height, directions, visited)
-  end
-
-  defp move(_pos, _dir, _grid, _width, _height, _directions, visited) do
+  defp move(_pos, _dir, _obstacles, _width, _height, visited) do
     visited
-  end
-
-  defp get_next(grid, row, col, dir, directions) do
-    {row_offset, col_offset} = Enum.at(directions, dir)
-    pos = {row + row_offset, col + col_offset}
-
-    char = Map.get(grid, pos, ".")
-    if char == "." do
-      {pos, dir}
-    else
-      if dir + 1 == length(directions) do
-        get_next(grid, row, col, 0, directions)
-      else
-        get_next(grid, row, col, dir + 1, directions)
-      end
-    end
-  end
-
-  defp find_char(grid, search) do
-    grid
-    |> Enum.filter(fn {_, char} -> char == search end)
-    |> Enum.map(fn {pos, _} -> pos end)
-  end
-
-  defp parse_row({row, y}, map) do
-    row
-    |> String.split("", trim: true)
-    |> Enum.with_index()
-    |> Enum.reduce(map, fn {col, x}, map ->
-      Map.put(map, {y, x}, col)
-    end)
   end
 
   defp get_input(sample) do
